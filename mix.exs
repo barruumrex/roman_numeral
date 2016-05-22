2a1431bd5e31b6d11bf9b6cb9353b5db18bce906
barruumrex <barruumrex@gmail.com>
2016-05-22 15:07:49 -0400

    Add credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -27,6 +27,9 @@ defmodule RomanNumeral.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.3.10", only: [:dev, :test]},
+      {:dialyxir, "~> 0.3.3", only: [:dev, :test]},
+    ]
   end
 end
